b27e7497242aa74591868554811c2d41d8ce592b
iamsdbhosale <sdbhosale@gmail.com>
2025-04-25 16:45:46 +0530

    new file:   Accesing_on_mobile_with_IPaddress.jpg        new file:   Localnetwork_with_IPaddress.mp4        new file:   ganache_local_to_ipaddress.txt        new file:   meta_expanded_browser_view.png        modified:   src/abis/EthSwap.json        modified:   src/abis/Migrations.json        modified:   src/abis/Token.json        modified:   truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,10 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      //host: "127.0.0.1",
+      //port: 7545,
+      host: "192.168.225.68",
+      port: 8545,
       network_id: "*" // Match any network id
     },
   },
